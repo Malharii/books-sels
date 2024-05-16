38c3f62b72a0b09869216c2d8d09bb1b6a4b6c98
Malharii <mrmalhari96@gmail.com>
2024-05-16 18:39:15 +0530

    adding controller

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,12 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
-
+const productsController = require("../controllers/products");
 const router = express.Router();
-const products = [];
 
 // /admin/add-product => GET
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
-});
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-exports.routes = router;
-exports.products = products;
+router.post("/add-product", productsController.postAddProduct);
+module.exports = router;
