32a9baa6ee7afe3080c0905cee78a3a7e122bbac
Malharii <mrmalhari96@gmail.com>
2024-05-16 14:48:25 +0530

    install ejs and creating ejs file

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,21 @@
 const path = require("path");
 
 const express = require("express");
-
+const adminData = require("./admin");
 const rootDir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
